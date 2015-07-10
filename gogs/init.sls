50a0a743885a47435b7e10c52facb509d885d9cb
Renaud Martinet <karouf@gmail.com>
2015-07-10 14:09:18 +0200

    Add init script to run gogs service

--- gogs/init.sls
+++ gogs/init.sls
@@ -1,2 +1,7 @@
 git:
   user.present
+
+/etc/rc.d/init.d/gogs:
+  file.managed:
+    - source: salt://gogs/files/init
+    - mode: 755
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -5,3 +5,10 @@ describe user('git') do
   it { should belong_to_group 'git' }
   it { should have_home_directory '/home/git' }
 end
+
+describe file('/etc/rc.d/init.d/gogs') do
+  it { should be_mode 755 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  its(:sha256sum) { should eq 'ebc9c582d36fd139bf742af6df4717c91ace0202d9766d077669f795efcf777a' }
+end
